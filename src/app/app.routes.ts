52419df5afe849eeaff550b4d1889fdb841bc8b2
adriano <adriano2015df@gmail.com>
2024-07-29 23:00:40 -0300

    incluão do footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes} from '@angular/router';
 import { HomeComponent } from '../pages/home/home.component';
+import { Falcon9Component } from '../pages/falcon-9/falcon-9.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'falcon-9', component: Falcon9Component}
 ];
